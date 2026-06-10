e73d15702f1a62aefdd9aef726f338339a66afeb
Craig Despain <iamcraigman@gmail.com>
2026-06-10 16:38:57 -0600

    Updated Trial Conversion query to accurately show data

--- models/intermediate/int_customer_conversions.sql
+++ models/intermediate/int_customer_conversions.sql
@@ -5,29 +5,35 @@ with subscriptions as (
 ordered_history as (
     select
         customer_id,
-        subscription_plan,
-        subscription_status,
-        valid_from_date,
-        valid_to_date,
+        plan_name,
+        status,
+        start_date,
+        end_date,
         -- Find the user's very first plan tier
-        first_value(subscription_plan) over (
+        first_value(plan_name) over (
             partition by customer_id 
-            order by valid_from_date asc
+            order by start_date asc
             rows between unbounded preceding and unbounded following
         ) as initial_plan,
+        -- Find the user's very first staus
+        first_value(status) over (
+            partition by customer_id 
+            order by start_date asc
+            rows between unbounded preceding and unbounded following
+        ) as initial_status,
         -- Get the plan details of their NEXT chronological subscription state
-        lead(subscription_plan) over (
+        lead(plan_name) over (
             partition by customer_id 
-            order by valid_from_date asc
+            order by start_date asc
         ) as next_plan,
-        lead(subscription_status) over (
+        lead(status) over (
             partition by customer_id 
-            order by valid_from_date asc
+            order by start_date asc
         ) as next_status,
         -- Row number to isolate their first lifecycle event block
         row_number() over (
             partition by customer_id 
-            order by valid_from_date asc
+            order by start_date asc
         ) as event_sequence
     from subscriptions
 ),
@@ -40,9 +46,9 @@ conversions_calculated as (
         next_status,
         case
             -- Scenario 1: Initial plan was a free trial, and the next record shows a valid plan upgrade
-            when initial_plan in ('Free / Trial', 'Trial', 'Basic') and next_plan in ('Pro', 'Enterprise') then 'Trial Upgraded'
-            -- Scenario 2: Initial plan was a trial, and the next record flags a cancellation status
-            when initial_plan in ('Free / Trial', 'Trial') and next_status = 'cancelled' then 'Trial Cancelled'
+            when initial_plan in ('Trial') and next_plan in ('Pro', 'Enterprise', 'Basic') then 'Trial Upgraded'
+            -- Scenario 2: Initial plan was a trial, and the intial record flags a cancellation status
+            when initial_plan in ('Trial') and initial_status = 'cancelled' then 'Trial Cancelled'
             -- Scenario 3: Ongoing or signed up directly to a paid tier
             else 'Direct Sign-up / Active'
         end as trial_conversion_cohort
